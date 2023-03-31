6ed3a9c1759643ea074e5d2bf7d5e53cc7f44a3d
claudiabarralesc <128436276+claudiabarralesc@users.noreply.github.com>
2023-03-31 11:40:28 -0300

    Primer commit

---
+++ Python semana1/holamundo.py
@@ -0,0 +1,6 @@
+sum=1+2
+print(sum)
+planets_in_solar_system = 8 # int, pluto used to be the 9th planet, but is too small
+distance_to_alpha_centauri = 4.367 # float, lightyears
+can_liftoff = True
+shuttle_landed_on_the_moon = "Apollo 11" #stringrint
